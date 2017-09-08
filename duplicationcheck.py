daa41368e449b3f10a3d160ee117f6bf70b667ba
ian lai <lai.kheng.yan@gmail.com>
2017-09-08 13:37:25 +0800

    init

--- duplicationcheck.py
+++ duplicationcheck.py
@@ -1,27 +1,35 @@
 import os
-
+from openpyxl import load_workbook
 # Retrieve current working directory (`cwd`)
-cwd = os.getcwd()
-
-print(cwd)
-
-print(os.listdir('.'))
-
-
+# cwd = os.getcwd()
+#
+# print(cwd)
+#
+# print(os.listdir('.'))
 
 
 
-# Import pandas
-import pandas as pd
 
-# Assign spreadsheet filename to `file`
-file = 'ner.xlsx'
+#
+# # Import pandas
+# import pandas as pd
+#
+# # Assign spreadsheet filename to `file`
+# file = 'ner.xlsx'
+#
+# # Load spreadsheet
+# xl = pd.ExcelFile(file)
+#
+# # Print the sheet names
+# print(xl.sheet_names)
+#
+# # Load a sheet into a DataFrame by name: df1
+# df1 = xl.parse('NER')
 
-# Load spreadsheet
-xl = pd.ExcelFile(file)
 
-# Print the sheet names
-print(xl.sheet_names)
+#loading in workbook
+wb = load_workbook('ner.xlsx')
 
-# Load a sheet into a DataFrame by name: df1
-df1 = xl.parse('NER')
+print("Sheets names:")
+#obtaining sheets names
+print(wb.get_sheet_names())

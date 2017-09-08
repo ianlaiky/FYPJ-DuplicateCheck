7a9554b7320f74622388a893428449566284d1f5
ian lai <lai.kheng.yan@gmail.com>
2017-09-08 15:22:21 +0800

    init

--- duplicationcheck.py
+++ duplicationcheck.py
@@ -1,35 +1,55 @@
 import os
 from openpyxl import load_workbook
-# Retrieve current working directory (`cwd`)
-# cwd = os.getcwd()
-#
-# print(cwd)
-#
-# print(os.listdir('.'))
+import pandas as pd
+import fnmatch
+import re
 
+# userWeb = input("Word duplication check: ")
 
-
-
-#
-# # Import pandas
-# import pandas as pd
-#
-# # Assign spreadsheet filename to `file`
-# file = 'ner.xlsx'
-#
-# # Load spreadsheet
-# xl = pd.ExcelFile(file)
-#
-# # Print the sheet names
-# print(xl.sheet_names)
-#
-# # Load a sheet into a DataFrame by name: df1
-# df1 = xl.parse('NER')
-
-
-#loading in workbook
+# loading in workbook
 wb = load_workbook('ner.xlsx')
 
 print("Sheets names:")
-#obtaining sheets names
-print(wb.get_sheet_names())
+# obtaining sheets names
+print(wb.get_sheet_names()[0])
+sheet = wb[wb.get_sheet_names()[0]]
+
+#
+data = sheet.values
+
+# print(next(data)[0:])
+columnlist =[]
+
+numbersss = 0
+for r in data:
+    # print(r[0])
+    columnlist.append(r[0])
+    numbersss += 1
+# print(columnlist)
+
+
+# regex test
+
+
+# for i in columnlist:
+#     print(i)
+
+for i in columnlist:
+    try:
+        if fnmatch.fnmatch(i,"*coll*"):
+            print(i)
+
+    except Exception:
+        pass
+
+
+
+
+
+
+print(numbersss)
+
+
+
+
+

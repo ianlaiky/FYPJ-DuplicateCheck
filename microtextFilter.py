e1586e9d161f628c6a8db4fff0e6c586df830114
ian lai <lai.kheng.yan@gmail.com>
2017-09-13 17:55:14 +0800

    init

--- microtextFilter.py
+++ microtextFilter.py
@@ -2,6 +2,7 @@ import re
 from openpyxl import load_workbook
 from collections import Counter
 
+
 def excelinput(filetoeopn, filecheckksheets, columnNo):
     columnlist = []
     # loading in workbook
@@ -15,8 +16,6 @@ def excelinput(filetoeopn, filecheckksheets, columnNo):
     #
     data = sheet.values
 
-
-
     for r in data:
         # print(r[0])
         if r[columnNo] is not None:
@@ -24,6 +23,7 @@ def excelinput(filetoeopn, filecheckksheets, columnNo):
             columnlist.append(r[columnNo])
 
     return columnlist
+
 
 # excelinput("datafiles\sgforums.xlsx",0,0)
 
@@ -38,83 +38,46 @@ class DataValue():
 
     def getitemCount(self):
         return self.countt
-    def setitemCounr(self,itemm):
-        self.item=itemm
+
+    def setitemCounr(self, itemm):
+        self.item = itemm
         return self.item
 
-listoflistobjects=[]
-listtt=[]
-listtt.append("testttttt")
-listtt.append(0)
-listoflistobjects.append(listtt)
-# def wordduplicationcheck(wordstocheck):
-#     wordcount=0
-#     templist=[]
-#     for i in listoflistobjects:
-#         if str(i[0])==wordstocheck:
-#             wordcount=wordcount+1
-#     if wordcount==0:
-#         if wordstocheck!="":
-#             templist.append(wordstocheck)
-#             templist.append(1)
-#             listoflistobjects.append(templist)
-#     else:
-#         for count,element in enumerate(listoflistobjects):
-#             if str(element[0])==wordstocheck:
-#                 tempcont = listoflistobjects[count][1]
-#                 listoflistobjects[count][1]=tempcont+1
 
-tempppppaarrrrr=[]
+tempppppaarrrrr = []
+
+
 def wordduplicationcheck(wordstocheck):
     tempppppaarrrrr.append(wordstocheck)
 
 
+f = open('test.txt', 'w', encoding="utf-8")
 
+counttttt = 0
 
-
-
-
-f = open('test.txt','w',encoding="utf-8")
-
-
-
-
-counttttt=0
-
-for i in excelinput("datafiles\sgforums.xlsx",0,0):
+for i in excelinput("datafiles\sgforums.xlsx", 0, 0):
     # print(i)
-    print("Currently scanning Line: "+str(counttttt))
-    counttttt=counttttt+1
-    i=str(i).replace('\n'," ")
-    i=str(i).replace('\\n'," ")
-    i=str(i).lower()
-    #do blank check to see if have space
-    if str(i).find(" ")!=-1:
-        for x in re.split(" |,",i):
+    print("Currently scanning Line: " + str(counttttt))
+    counttttt = counttttt + 1
+    i = str(i).replace('\n', " ")
+    i = str(i).replace('\\n', " ")
+    i = str(i).lower()
+    # do blank check to see if have space
+    if str(i).find(" ") != -1:
+        for x in re.split(" |,", i):
             # print(x)
             # f.writelines(x+"\n")
             wordduplicationcheck(str(x).strip())
     else:
         wordduplicationcheck(str(i).strip())
-        # f.writelines(str(i) + "\n")
-    # except:
-    #     pass
-listoflistobjects.sort(key = lambda x: x[1],reverse=True)
-
-# obsolete
-# for h in listoflistobjects:
-#     f.writelines(str(h[0])+"\t\t"+str(h[1])+"\n")
 
 my_dict = Counter(tempppppaarrrrr)
-# f.writelines(str(my_dict) + "\n")
-# print(my_dict)
+
 del my_dict[' ']
 del my_dict['']
 
 for fg in my_dict:
-    f.writelines(fg+"\t\t"+str(my_dict[fg])+"\n")
+    f.writelines(fg + "\t\t" + str(my_dict[fg]) + "\n")
 print("Done")
-
-# print(listoflistobjects)
 
 f.close()

bb0d0dcbaf32d55e2497e65bc5dca4564d571955
ian lai <lai.kheng.yan@gmail.com>
2017-09-22 10:16:24 +0800

    init

---
+++ test.py
@@ -0,0 +1,55 @@
+
+import enchant
+d = enchant.Dict("en_US")
+print(d.check("Hello"))
+
+
+# import re
+# from openpyxl import load_workbook
+# from collections import Counter
+#
+#
+# def excelinput(filetoeopn, filecheckksheets, columnNo):
+#     columnlist = []
+#     # loading in workbook
+#     wb = load_workbook(filetoeopn)
+#
+#     print("Sheets names:")
+#     # obtaining sheets names
+#     print(wb.get_sheet_names())
+#     sheet = wb[wb.get_sheet_names()[filecheckksheets]]
+#
+#     #
+#     data = sheet.values
+#
+#     for r in data:
+#         # print(r[0])
+#         if r[columnNo] is not None:
+#             # print(r[columnNo])
+#             columnlist.append(r[columnNo])
+#
+#     return columnlist
+#
+# for i in excelinput("datafiles\sgforums.xlsx", 0, 0):
+#     if re.search("don",str(i))!="None":
+#         print(i)
+#
+# x="(1)"
+# x1="2)dsad"
+# x2="3) dasasad"
+#
+# print(re.match("^[0-9]\)+$",x))
+#
+# if str(re.match("^\([0-9]\)+$", x)) != "None":
+#     print("false")
+
+
+# y="text,,,0"
+# x="(pp)"
+# x1=""
+# x2=""
+# print(y.split(","))
+#
+# print(x[x.index(y.split(",")[1])+int(y.split(",")[3]):x.index(y.split(",")[2])])
+
+

46b4c411b7359fc5709052c17411a45df0d680f6
Ian <152772a@mymail.nyp.edu.sg>
2017-09-23 22:11:59 +0800

    init

--- duplicationcheck.py
+++ duplicationcheck.py
@@ -30,6 +30,7 @@ def excelinput(filetoeopn, filecheckksheets, columnNo):
     print("Sheets names:")
     # obtaining sheets names
     print(wb.get_sheet_names())
+    print("Sheets Loaded: "+wb.get_sheet_names()[filecheckksheets])
     sheet = wb[wb.get_sheet_names()[filecheckksheets]]
 
     #

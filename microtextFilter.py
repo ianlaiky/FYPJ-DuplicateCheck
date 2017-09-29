8bb8c7da36b813c4a88696c2568ba331d34415b3
ian lai <lai.kheng.yan@gmail.com>
2017-09-29 08:57:37 +0800

    init

--- microtextFilter.py
+++ microtextFilter.py
@@ -427,6 +427,7 @@ for i in excelinput(forumdataforreading, 0, 0):
     i = str(i).replace('', " ")
     i = str(i).replace('', " ")
     i = str(i).replace('', " ")
+    i = str(i).replace('', " ")
     i = str(i).replace('…', '...')
 
     # i = str(i).replace('/', " / ")
--- sentenceAssignment/wordsSentenceAssignment.py
+++ sentenceAssignment/wordsSentenceAssignment.py
@@ -411,6 +411,7 @@ for i in excelinput(forumdataforreading, 0, 0):
     i = str(i).replace('', " ")
     i = str(i).replace('', " ")
     i = str(i).replace('', " ")
+    i = str(i).replace('', " ")
     i = str(i).replace('…', '...')
 
     # i = str(i).replace('/', " / ")

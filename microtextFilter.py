2cd81b9f43bcfaa89ccd5aa86dbdcd3627cf7e3a
ian lai <lai.kheng.yan@gmail.com>
2017-10-06 18:30:43 +0800

    init

--- microtextFilter.py
+++ microtextFilter.py
@@ -432,6 +432,7 @@ for i in excelinput(forumdataforreading, 0, 0):
     i = str(i).replace('', " ")
     i = str(i).replace('', " ")
     i = str(i).replace('', " ")
+    i = str(i).replace('', " ")
 
     i = str(i).replace('…', '...')
 
--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 import re
+from openpyxl import load_workbook
+
+import fnmatch
+
 
 # import enchant
 #
@@ -92,31 +96,31 @@ import re
 #     print(x.index(firstword))
 #     print("Found")
 
-testarr = []
-stopword = ["a","i'll","the"]
-
-x = "whiat the games ass | a d asd the sd s/ads i'll fdsf ds.fcdsfs,     fsdf ds     fdsf"
-
-sentence=""
-for te in x.split(" "):
-    if str(te)!="":
-        if str(te)!=" ":
-            wordcount = 0
-            for qwe in stopword:
-
-                print(te.strip())
-                if str(re.match("^("+str(qwe)+")$", str(te).strip()))!="None":
-                    temp=str(te).replace(qwe, "")
-                    sentence=str(sentence)+" "+str(temp).strip()
-                    wordcount=wordcount+1
-            if wordcount==0:
-                sentence = str(sentence) + " " + str(te).strip()
-            else:
-                print("gdfg")
-
-
-
-print(sentence)
+# testarr = []
+# stopword = ["a","i'll","the"]
+#
+# x = "whiat the games ass | a d asd the sd s/ads i'll fdsf ds.fcdsfs,     fsdf ds     fdsf"
+#
+# sentence=""
+# for te in x.split(" "):
+#     if str(te)!="":
+#         if str(te)!=" ":
+#             wordcount = 0
+#             for qwe in stopword:
+#
+#                 print(te.strip())
+#                 if str(re.match("^("+str(qwe)+")$", str(te).strip()))!="None":
+#                     temp=str(te).replace(qwe, "")
+#                     sentence=str(sentence)+" "+str(temp).strip()
+#                     wordcount=wordcount+1
+#             if wordcount==0:
+#                 sentence = str(sentence) + " " + str(te).strip()
+#             else:
+#                 print("gdfg")
+#
+#
+#
+# print(sentence)
 
 
 
@@ -133,3 +137,63 @@ print(sentence)
 
 
 # print(testarr)
+
+
+def excelinput(filetoeopn, filecheckksheets, columnNo):
+    columnlist = []
+    # loading in workbook
+    wb = load_workbook(filetoeopn)
+
+    print("Sheets names:")
+    # obtaining sheets names
+    print(wb.get_sheet_names())
+    print("Sheets Loaded: " + wb.get_sheet_names()[filecheckksheets])
+    sheet = wb[wb.get_sheet_names()[filecheckksheets]]
+
+    #
+    data = sheet.values
+
+    # print(next(data)[0:])
+
+    for r in data:
+        # print(r[0])
+        columnlist.append(r[columnNo])
+
+    return columnlist
+
+counttttt = 0
+
+currentFileNo=0
+
+for i in excelinput("datafiles\sgforums.xlsx", 0, 0):
+    if str(i) != "None":
+
+        print("Currently scanning Line: " + str(counttttt))
+        counttttt = counttttt + 1
+        i = str(i).replace('\n', " ")
+        i = str(i).replace('\\n', " ")
+        i = str(i).replace('', " ")
+        i = str(i).replace('', " ")
+        i = str(i).replace('', " ")
+        i = str(i).replace('', " ")
+        i = str(i).replace('', " ")
+        i = str(i).replace('', " ")
+        i = str(i).replace('', " ")
+        i = str(i).replace('', " ")
+        i = str(i).replace('', " ")
+        i = str(i).replace('', " ")
+        i = str(i).replace('', " ")
+        i = str(i).replace('', " ")
+        i = str(i).replace('', " ")
+        i = str(i).replace('', " ")
+        i = str(i).replace('', " ")
+        i = str(i).replace('', " ")
+        i = str(i).replace('', " ")
+
+        i = str(i).replace('…', '...')
+        print(i)
+
+        f = open("sentencesInFiles\sentence_"+str(currentFileNo)+".txt","w", encoding="utf-8")
+        f.writelines(str(i))
+        f.close()
+        currentFileNo=int(currentFileNo)+1

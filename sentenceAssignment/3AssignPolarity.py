27cb2caffd4f0e9bc87515c75ac2d1856b7d49ca
ian lai <lai.kheng.yan@gmail.com>
2017-10-12 14:32:15 +0800

    init

--- sentenceAssignment/3AssignPolarity.py
+++ sentenceAssignment/3AssignPolarity.py
@@ -1,47 +1,97 @@
-
-f = open("wordsSentenceAssignment\singlish_polarity_SVMresults_conf.txt",'r',encoding="utf-8")
+f = open("wordsSentenceAssignment\singlish_polarity_SVMresults_conf.txt", 'r', encoding="utf-8")
 file = f.readlines()
 f.close()
 
-polarityDict={}
+polarityDict = {}
 
 for i in file:
-    if str(i).strip()!="":
-        if str(i).strip()!=" ":
+    if str(i).strip() != "":
+        if str(i).strip() != " ":
             # print(str(i).strip())
 
-            print(str(i).strip()[str(i).find("sentence_")+9:str(i).find(".txt")])
-            print(str(i).strip()[str(i).find("conf(pos)=")+10:str(i).find("conf(neg)")-1])
-            polarityDict[str(i).strip()[str(i).find("sentence_")+9:str(i).find(".txt")]]=str(i).strip()[str(i).find("conf(pos)=")+10:str(i).find("conf(neg)")-1]
+            print(str(i).strip()[str(i).find("sentence_") + 9:str(i).find(".txt")])
+            print(str(i).strip()[str(i).find("conf(pos)=") + 10:str(i).find("conf(neg)") - 1])
+            polarityDict[str(i).strip()[str(i).find("sentence_") + 9:str(i).find(".txt")]] = str(i).strip()[str(i).find(
+                "conf(pos)=") + 10:str(i).find("conf(neg)") - 1]
 
 # print(polarityDict[str("2")])
 
 
 
-fout = open("wordsSentenceAssignment\sentencesAssignment.txt",'r',encoding="utf-8")
-sentence=fout.readlines()
+fout = open("wordsSentenceAssignment\sentencesAssignment.txt", 'r', encoding="utf-8")
+sentence = fout.readlines()
 fout.close()
 
+fintake = open("wordsSentenceAssignment\sentencesAssignmentWithPolarity.txt", 'w', encoding="utf-8")
 
-fintake=open("wordsSentenceAssignment\sentencesAssignmentWithPolarity.txt",'w',encoding="utf-8")
+dictforword = {}
 
-linescount=0
+linescount = 0
 for o in sentence:
-    if str(o).strip()!="":
-        if str(o).strip()!=" ":
+    if str(o).strip() != "":
+        if str(o).strip() != " ":
             # print(str(o).strip())
             print(str(o).strip()[str(o).index("|") + 2:])
+            # print(str(o).strip()[:str(o).index("|") -1])
             print(polarityDict[str(linescount)])
 
-            if float(polarityDict[str(linescount)])>0.7 or float(polarityDict[str(linescount)])<0.3:
-                fintake.writelines(str(o).strip()+"\n")
-                fintake.writelines("*High Polarity*"+"\n\n")
+            if str(o).strip()[:str(o).index("|") - 1] not in dictforword:
+                dictforword[str(o).strip()[:str(o).index("|") - 1]] = "0/0"
+            percent = dictforword[str(o).strip()[:str(o).index("|") - 1]]
+            firstpercent = str(percent[:str(percent).index("/")])
+            secondpercent = str(percent[int(str(percent).index("/")) + 1:])
+            if float(polarityDict[str(linescount)]) > 0.7 or float(polarityDict[str(linescount)]) < 0.3:
+
+                secondpercent=int(secondpercent)+1
+
+
+
+                # fintake.writelines(str(o).strip() + "\n")
+                # fintake.writelines("*High Polarity*" + "\n\n")
                 # fintake.writelines("Negative Polarity: "+str(polarityDict[str(linescount)])+"\n\n")
             else:
-                fintake.writelines(str(o).strip() + "\n")
-                fintake.writelines("Netural Polarity" + "\n\n")
+                firstpercent = int(firstpercent) + 1
+                secondpercent = int(secondpercent) + 1
+            # fintake.writelines(str(o).strip() + "\n")
+            # fintake.writelines("Netural Polarity" + "\n\n")
+            dictforword[str(o).strip()[:str(o).index("|") - 1]]=str(firstpercent)+"/"+str(secondpercent)
 
-            linescount=int(linescount)+1
 
+            linescount = int(linescount) + 1
+            print(linescount)
+
+listofwordstosave=[]
+
+for x in dictforword:
+    per = dictforword[x]
+    firstper= per[:str(per).index("/")]
+    secper= per[int(str(per).index("/"))+1:]
+    if int(firstper)/int(secper) >0.6:
+        listofwordstosave.append(str(x))
+        print(x)
+    else:
+        print("Neutral")
+        print(x)
+# print(dictforword["fokkers"])
 
 fintake.close()
+finsave = open("wordsSentenceAssignment\FilteredsentencesAssignmentWithPolarity.txt", 'w', encoding="utf-8")
+
+for oi in sentence:
+    if str(oi).strip() != "":
+        if str(oi).strip() != " ":
+            for oii in listofwordstosave:
+                if str(oi).strip()[:str(oi).index("|") - 1]==str(oii):
+                    finsave.writelines(oi+"\n")
+
+
+
+
+
+
+
+
+
+finsave.close()
+
+
--- test.py
+++ test.py
@@ -139,67 +139,72 @@ import fnmatch
 # print(testarr)
 
 
-def excelinput(filetoeopn, filecheckksheets, columnNo):
-    columnlist = []
-    # loading in workbook
-    wb = load_workbook(filetoeopn)
+# def excelinput(filetoeopn, filecheckksheets, columnNo):
+#     columnlist = []
+#     # loading in workbook
+#     wb = load_workbook(filetoeopn)
+#
+#     print("Sheets names:")
+#     # obtaining sheets names
+#     print(wb.get_sheet_names())
+#     print("Sheets Loaded: " + wb.get_sheet_names()[filecheckksheets])
+#     sheet = wb[wb.get_sheet_names()[filecheckksheets]]
+#
+#     #
+#     data = sheet.values
+#
+#     # print(next(data)[0:])
+#
+#     for r in data:
+#         # print(r[0])
+#         columnlist.append(r[columnNo])
+#
+#     return columnlist
+#
+# counttttt = 0
+#
+# currentFileNo=0
+#
+# for i in excelinput("datafiles\sgforums.xlsx", 0, 0):
+#     if str(i) != "None":
+#
+#         print("Currently scanning Line: " + str(counttttt))
+#         counttttt = counttttt + 1
+#         i = str(i).replace('\n', " ")
+#         i = str(i).replace('\\n', " ")
+#         i = str(i).replace('', " ")
+#         i = str(i).replace('', " ")
+#         i = str(i).replace('', " ")
+#         i = str(i).replace('', " ")
+#         i = str(i).replace('', " ")
+#         i = str(i).replace('', " ")
+#         i = str(i).replace('', " ")
+#         i = str(i).replace('', " ")
+#         i = str(i).replace('', " ")
+#         i = str(i).replace('', " ")
+#         i = str(i).replace('', " ")
+#         i = str(i).replace('', " ")
+#         i = str(i).replace('', " ")
+#         i = str(i).replace('', " ")
+#         i = str(i).replace('', " ")
+#         i = str(i).replace('', " ")
+#         i = str(i).replace('', " ")
+#
+#         i = str(i).replace('…', '...')
+#         print(i)
+#
+#         f = open("..\sentencesInFiles\sentence_"+str(currentFileNo)+".txt","w", encoding="utf-8")
+#         f.writelines(str(i))
+#         f.close()
+#         currentFileNo=int(currentFileNo)+1
+#
+# x = "0.78"
+#
+# x=float(x)+1
+#
+# print(x)
 
-    print("Sheets names:")
-    # obtaining sheets names
-    print(wb.get_sheet_names())
-    print("Sheets Loaded: " + wb.get_sheet_names()[filecheckksheets])
-    sheet = wb[wb.get_sheet_names()[filecheckksheets]]
+x = "0/2"
 
-    #
-    data = sheet.values
-
-    # print(next(data)[0:])
-
-    for r in data:
-        # print(r[0])
-        columnlist.append(r[columnNo])
-
-    return columnlist
-
-counttttt = 0
-
-currentFileNo=0
-
-for i in excelinput("datafiles\sgforums.xlsx", 0, 0):
-    if str(i) != "None":
-
-        print("Currently scanning Line: " + str(counttttt))
-        counttttt = counttttt + 1
-        i = str(i).replace('\n', " ")
-        i = str(i).replace('\\n', " ")
-        i = str(i).replace('', " ")
-        i = str(i).replace('', " ")
-        i = str(i).replace('', " ")
-        i = str(i).replace('', " ")
-        i = str(i).replace('', " ")
-        i = str(i).replace('', " ")
-        i = str(i).replace('', " ")
-        i = str(i).replace('', " ")
-        i = str(i).replace('', " ")
-        i = str(i).replace('', " ")
-        i = str(i).replace('', " ")
-        i = str(i).replace('', " ")
-        i = str(i).replace('', " ")
-        i = str(i).replace('', " ")
-        i = str(i).replace('', " ")
-        i = str(i).replace('', " ")
-        i = str(i).replace('', " ")
-
-        i = str(i).replace('…', '...')
-        print(i)
-
-        f = open("..\sentencesInFiles\sentence_"+str(currentFileNo)+".txt","w", encoding="utf-8")
-        f.writelines(str(i))
-        f.close()
-        currentFileNo=int(currentFileNo)+1
-
-x = "0.78"
-
-x=float(x)+1
-
-print(x)
+print(x[:str(x).index("/")])
+print(x[str(x).index("/")+1:])

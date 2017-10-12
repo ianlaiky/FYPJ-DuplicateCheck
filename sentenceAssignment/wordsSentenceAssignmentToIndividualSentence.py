602fdbe844322d6108cfe28172033b586635bedc
ian lai <lai.kheng.yan@gmail.com>
2017-10-12 10:55:37 +0800

    init

--- sentenceAssignment/wordsSentenceAssignmentToIndividualSentence.py
+++ sentenceAssignment/wordsSentenceAssignmentToIndividualSentence.py
@@ -3,10 +3,16 @@ f = open("wordsSentenceAssignment\sentencesAssignment.txt",'r', encoding="utf-8"
 files = f.readlines()
 f.close()
 
+
+filecount=0
 for i in files:
     if str(i).strip() !="":
         if str(i).strip() !=" ":
             # print(str(i).strip())
 
-            print(str(i)[str(i).index("|")+2:])
+            print(str(i).strip()[str(i).index("|")+2:])
+            fi = open("..\..\sentencesInFiles\sentence_"+str(filecount)+".txt",'w', encoding="utf-8")
+            fi.writelines(str(i).strip()[str(i).index("|")+2:])
+            fi.close()
+            filecount = int(filecount)+1
 

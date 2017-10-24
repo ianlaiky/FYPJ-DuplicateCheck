6c59029849eb5f225199ccd4e247842e10eedbf9
Ian <152772a@mymail.nyp.edu.sg>
2017-10-24 10:44:43 +0800

    i

--- dataMining/dataMining.py
+++ dataMining/dataMining.py
@@ -6,12 +6,13 @@ import os
 headers = {'User-agent': 'Mozilla/5.0'}
 
 # userWeb = input("Website: ")
-userWeb = "http://www.timesbusinessdirectory.com/company/list/all/1"
+userWeb = "http://www.timesbusinessdirectory.com/company/list/all/"
+interation=1
 web2="http://www.timesbusinessdirectory.com/company"
 
-r = requests.get(userWeb, headers=headers)
-data = r.text
-soup = BeautifulSoup(data, "lxml")
+# r = requests.get(userWeb, headers=headers)
+# data = r.text
+# soup = BeautifulSoup(data, "lxml")
 
 # print(soup.find('class="container"'))
 # print(soup.find_all('div',{"class": "contenth3"}))
@@ -19,33 +20,20 @@ soup = BeautifulSoup(data, "lxml")
 
 textlist={}
 
+for links in range(946):
+    print(str(userWeb)+str(links))
 
-for i in soup.find_all('div',{"class": "contenth3"}):
-    print(i.text)
-    print(i.find('a')['href'])
-    textlist[str(i.text).strip()]=str(i.find('a')['href'])
+    r = requests.get(str(userWeb)+str(links), headers=headers)
+    data = r.text
+    soup = BeautifulSoup(data, "lxml")
+    for i in soup.find_all('div',{"class": "contenth3"}):
+        print(i.text)
+        print(i.find('a')['href'])
+        textlist[str(i.text).strip()]=str(i.find('a')['href'])
 
 
 print(textlist)
+print(len(textlist))
 
 
 
-# for link in soup.find_all("container"):
-#     print(link)
-
-# image = link.get("src")
-# imag = image.replace("image", "original")
-# # print(imag.find("http"))
-# if(imag.find("http://cfile")==0):
-#     print(imag)
-#
-#     imgSplit=os.path.split(imag)
-#     if (imgSplit[1][-3:]!="gif"):
-#         imgNam = imgSplit[1]+".jpg"
-#     else:
-#         imgNam = imgSplit[1]
-#     r2=requests.get(imag,headers=headers)
-#     # print(imgNam)
-#     with open(imgNam,"wb") as f:
-#         f.write(r2.content)
-#

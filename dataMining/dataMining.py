2c746999a08792ff9bbb89d503de15068a3beafe
ian lai <lai.kheng.yan@gmail.com>
2017-10-24 17:28:11 +0800

    init

--- dataMining/dataMining.py
+++ dataMining/dataMining.py
@@ -3,6 +3,17 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
+
+
+try:
+
+    os.mkdir("extractedData")
+except:
+    pass
+f = open("extractedData\ExtractedData.txt", 'w', encoding="utf8")
+
+
+# f.writelines(str("dsd") + "\n\n")
 headers = {'User-agent': 'Mozilla/5.0'}
 
 # userWeb = input("Website: ")
@@ -30,10 +41,20 @@ for links in range(946):
         print(i.text)
         print(i.find('a')['href'])
         textlist[str(i.text).strip()]=str(i.find('a')['href'])
+        f.writelines(str(i.text).strip()+"\n")
 
 
 print(textlist)
 print(len(textlist))
 
+
+
+
+
+# for opppp in textlist:
+#     f.writelines(str(opppp)+"\n\n")
+
+
+f.close()
 
 

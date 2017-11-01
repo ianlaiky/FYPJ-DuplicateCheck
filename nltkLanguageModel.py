4079ceb0e5fce8bff24bd15f6aeb45706f15701c
ian lai <lai.kheng.yan@gmail.com>
2017-11-01 20:40:35 +0800

    init

--- nltkLanguageModel.py
+++ nltkLanguageModel.py
@@ -3,8 +3,36 @@ from nltk.corpus import stopwords
 word =" Tiagong from reliable source related to peter that peter doesn't allow her to step into the amk home liao. That's why until now havent visit the two past tense dotters"
 
 
-# print(stopwords.fileids())
 
+
+
+
+def pythonFile(filetoopen, startreadArea, endReadArea, indextoadd, wordstoignore):
+    pyarr = []
+    f = open(filetoopen, 'r', encoding="utf8")
+    message = f.readlines()
+
+    # print(message)
+    f.close()
+    # print(message)
+    for myString in message:
+        # print(myString)
+
+        try:
+            if myString.find(wordstoignore) == -1:
+                if startreadArea != "":
+                    wewe=myString[myString.index(startreadArea) + indextoadd:myString.index(endReadArea)].replace("_"," ")
+                    for insert in str(wewe).split(" "):
+                        pyarr.append(insert)
+                else:
+                    pyarr.append(myString.replace("\n", ""))
+        except:
+            pass
+    return pyarr
+
+
+senticNetWords=pythonFile("filesdb\senticnet.py","['","']",2,"-----nil--------")
+print(set(senticNetWords))
 
 languages_ratios = {}
 tokens = wordpunct_tokenize(word)
@@ -13,15 +41,20 @@ words = [word.lower() for word in tokens]
 
 
 # list of sets of stopwords for each language
-dictOfSetsOfStopwords=[]
+dictOfSetsOfStopwords={}
+languages_ratios={}
 
-# for language in stopwords.fileids():
-#     stopwords_set = set(stopwords.words(language))
-#     dictOfSetsOfStopwords[language]=stopwords_set
-
-
-
-
+dictOfSetsOfStopwords["singlish"]=set(senticNetWords)
+for language in stopwords.fileids():
+    stopwords_set = set(stopwords.words(language))
+    dictOfSetsOfStopwords[language]=stopwords_set
 
 
-# print(languages_ratios)
+for eachLan in dictOfSetsOfStopwords:
+    # print(dictOfSetsOfStopwords[eachLan])
+    corpusSet = set(words)
+    common_elements = corpusSet.intersection(dictOfSetsOfStopwords[eachLan])
+    languages_ratios[eachLan]=len(common_elements)
+
+
+print(languages_ratios)

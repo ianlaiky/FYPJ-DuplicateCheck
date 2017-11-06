f8da28bd28dd7522a94f81d557177a8534dfba8f
ian lai <lai.kheng.yan@gmail.com>
2017-11-06 16:47:29 +0800

    init

--- dataMining/ranker.com.py
+++ dataMining/ranker.com.py
@@ -3,14 +3,13 @@ import urllib.request, json
 
 
 f = open('dataset.txt', 'w', encoding="utf-8")
-
-
-with urllib.request.urlopen("https://cache-api.ranker.com/lists/161641/items?limit=50&offset=100&include=votes,wikiText,rankings,openListItemContributors&propertyFetchType=ALL&liCacheKey=null") as url:
+url="https://cache-api.ranker.com/lists/161641/items?limit=50&offset=150&include=votes,wikiText,rankings,openListItemContributors&propertyFetchType=ALL&liCacheKey=null"
+with urllib.request.urlopen(url) as url:
     data = json.loads(url.read().decode())
 
 
     for index,ele in enumerate(data["listItems"]):
 
         print(data["listItems"][index]["node"]["name"])
-        f.writelines(str(data["listItems"][index]["node"]["name"]))
+        f.writelines(str(data["listItems"][index]["node"]["name"])+"\n")
 f.close()

477d1041d30fa933af67f6954d2ce536b453ba4e
ian lai <lai.kheng.yan@gmail.com>
2017-11-22 20:41:05 +0800

    init

--- microtextNERDuplicateChecker161117/Checker2.0.py
+++ microtextNERDuplicateChecker161117/Checker2.0.py
@@ -158,9 +158,9 @@ for index2, dasdf in enumerate(listarrayOfFilesData):
                 # print(currCol2)
                 # print(currCol3)
 
-                newCol1 = str(currCol1) + ", "+str(listarrayOfFilesData[index2][index13][1][index1])
-                newCol2 = str(currCol2) + ", "+str(listarrayOfFilesData[index2][index13][2][index1])
-                newCol3 = str(currCol3) + ", "+str(listarrayOfFilesData[index2][index13][3][index1])
+                newCol1 = str(currCol1) + ", "+str(listarrayOfFilesData[index2][1][index13])
+                newCol2 = str(currCol2) + ", "+str(listarrayOfFilesData[index2][2][index13])
+                newCol3 = str(currCol3) + ", "+str(listarrayOfFilesData[index2][3][index13])
 
                 dictOfObj[str(readin).lower()]=Data(str(currCol0),str(newCol1),str(newCol2),str(newCol3))
 
@@ -172,10 +172,10 @@ for index2, dasdf in enumerate(listarrayOfFilesData):
                 input()
                 # print(listarrayOfFilesData[index2][index13][0])
                 # print(listarrayOfFilesData[index2][index13][0][index1])
-                print(str(listarrayOfFilesData[index2][index13][0][index1]))
-                print(str(listarrayOfFilesData[index2][index13][3][index1]))
+                print(str(listarrayOfFilesData[index2][1][index13]))
+                print(str(listarrayOfFilesData[index2][2][index13]))
 
-                dictOfObj[str(readin).lower()] = Data(str(listarrayOfFilesData[index2][index13][0][index1]), str(listarrayOfFilesData[index2][index13][1][index1]), str(listarrayOfFilesData[index2][index13][2][index1]), str(listarrayOfFilesData[index2][index13][3][index1]))
+                dictOfObj[str(readin).lower()] = Data(str(listarrayOfFilesData[index2][0][index13]), str(listarrayOfFilesData[index2][1][index13]), str(listarrayOfFilesData[index2][2][index13]), str(listarrayOfFilesData[index2][3][index13]))
 
                 print("fdsdsfdsfdsfdsfds")
 
